964405359b70bf0fa092c92cbf19310df2ee7742
Leung Yau Ming <leungyauming2005@gmail.com>
2021-06-24 16:27:52 +0800

    create JWS header parameter constants

---
+++ common/jws/header_params.go
@@ -0,0 +1,15 @@
+package jws
+
+const (
+	HeaderParamAlg     = "alg"
+	HeaderParamJKU     = "jku"
+	HeaderParamJWK     = "jwk"
+	HeaderParamKID     = "kid"
+	HeaderParamX5U     = "x5u"
+	HeaderParamX5C     = "x5c"
+	HeaderParamX5T     = "x5t"
+	HeaderParamX5TS256 = "x5t#S256"
+	HeaderParamTyp     = "typ"
+	HeaderParamCty     = "cty"
+	HeaderParamCrit    = "crit"
+)
