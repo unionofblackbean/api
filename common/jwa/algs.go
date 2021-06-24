6fcd80ee180080062621b9fdedd3220926a60a29
Leung Yau Ming <leungyauming2005@gmail.com>
2021-06-24 16:26:21 +0800

    create JWA algorithm constants

---
+++ common/jwa/algs.go
@@ -0,0 +1,21 @@
+package jwa
+
+const (
+	AlgHS256 = "HS256"
+	AlgHS384 = "HS384"
+	AlgHS512 = "HS512"
+
+	AlgRS256 = "RS256"
+	AlgRS384 = "RS384"
+	AlgRS512 = "RS512"
+
+	AlgES256 = "ES256"
+	AlgES384 = "ES384"
+	AlgES512 = "ES512"
+
+	AlgPS256 = "PS256"
+	AlgPS384 = "PS384"
+	AlgPS512 = "PS512"
+
+	AlgNONE = "none"
+)
