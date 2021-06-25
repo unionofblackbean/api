8230c0bf477f4e106d08cde58450df5e4bd126c6
Leung Yau Ming <leungyauming2005@gmail.com>
2021-06-25 18:23:08 +0800

    replace string literals in tests to constants

--- common/jws/header_test.go
+++ common/jws/header_test.go
@@ -10,34 +10,34 @@ import (
 func TestHeader_Get(t *testing.T) {
 	h := NewHeader()
 
-	assert.Nil(t, h.Get("alg"))
-	h.Set("alg", "RS256")
-	assert.NotNil(t, h.Get("alg"))
+	assert.Nil(t, h.Get(HeaderParamAlg))
+	h.Set(HeaderParamAlg, jwa.AlgRS256)
+	assert.NotNil(t, h.Get(HeaderParamAlg))
 }
 
 func TestHeader_Set(t *testing.T) {
 	h := NewHeader()
 
-	h.Set("alg", "RS256")
-	assert.True(t, h.Exists("alg"))
+	h.Set(HeaderParamAlg, jwa.AlgRS256)
+	assert.True(t, h.Exists(HeaderParamAlg))
 }
 
 func TestHeader_Remove(t *testing.T) {
 	h := NewHeader()
 
-	h.Set("alg", "RS256")
-	assert.True(t, h.Exists("alg"))
+	h.Set(HeaderParamAlg, jwa.AlgRS256)
+	assert.True(t, h.Exists(HeaderParamAlg))
 
-	h.Remove("alg")
-	assert.False(t, h.Exists("alg"))
+	h.Remove(HeaderParamAlg)
+	assert.False(t, h.Exists(HeaderParamAlg))
 }
 
 func TestHeader_Exists(t *testing.T) {
 	h := NewHeader()
 
-	assert.False(t, h.Exists("alg"))
-	h.Set("alg", "RS256")
-	assert.True(t, h.Exists("alg"))
+	assert.False(t, h.Exists(HeaderParamAlg))
+	h.Set(HeaderParamAlg, jwa.AlgRS256)
+	assert.True(t, h.Exists(HeaderParamAlg))
 }
 
 func TestHeader_Encode(t *testing.T) {
