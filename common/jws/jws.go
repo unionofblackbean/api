a7b99522ca144132ea7774505a6feb0b07dce0d5
Leung Yau Ming <leungyauming2005@gmail.com>
2021-06-26 12:20:19 +0800

    create jws.JWS.encodeHeaderPayload method

--- common/jws/jws.go
+++ common/jws/jws.go
@@ -1,5 +1,10 @@
 package jws
 
+import (
+	"fmt"
+	"github.com/leungyauming/api/common/pools"
+)
+
 type JWS struct {
 	Header  Header
 	Payload Payload
@@ -10,4 +15,26 @@ func New() *JWS {
 		Header:  NewHeader(),
 		Payload: NewPayload(),
 	}
+}
+
+func (jws *JWS) encodeHeaderPayload() ([]byte, error) {
+	headerEncoded, err := jws.Header.Encode()
+	if err != nil {
+		return nil, fmt.Errorf("failed to encode header -> %v", err)
+	}
+
+	payloadEncoded, err := jws.Payload.Encode()
+	if err != nil {
+		return nil, fmt.Errorf("failed to encode payload -> %v", err)
+	}
+
+	buf := pools.GetBytesBuffer()
+	buf.Write(headerEncoded)
+	buf.WriteRune('.')
+	buf.Write(payloadEncoded)
+
+	headerPayloadEncoded := buf.Bytes()
+	pools.PutBytesBuffer(buf)
+
+	return headerPayloadEncoded, nil
 }
