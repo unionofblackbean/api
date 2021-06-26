e37c48ac6c0b4354110baed5fdd180cf5f60867c
Leung Yau Ming <leungyauming2005@gmail.com>
2021-06-26 15:55:36 +0800

    create tests for jws.JWS.Sign using HS algorithms with []byte secret

--- common/jws/jws_test.go
+++ common/jws/jws_test.go
@@ -17,38 +17,77 @@ func TestNew(t *testing.T) {
 	assert.NotNil(t, jws.Payload)
 }
 
-func TestJWS_Sign_HS256(t *testing.T) {
+var hsSecretString = "thisissecret"
+
+func TestJWS_Sign_HS256_1(t *testing.T) {
 	jws := New()
 	jws.Header.Set(HeaderParamAlg, jwa.AlgHS256)
 	jws.Header.Set(HeaderParamTyp, jwt.TypJWT)
 
 	jws.Payload.Set("role", "admin")
 
-	sign, err := jws.Sign("thisissecret")
+	sign, err := jws.Sign(hsSecretString)
 	assert.Nil(t, err)
 	assert.Equal(t, []byte("300qEKK0Iibf-_t8rXzVDdBPX-xEXT-9v_sFJ2X4zYU"), sign)
 }
 
-func TestJWS_Sign_HS384(t *testing.T) {
+func TestJWS_Sign_HS256_2(t *testing.T) {
+	jws := New()
+	jws.Header.Set(HeaderParamAlg, jwa.AlgHS256)
+	jws.Header.Set(HeaderParamTyp, jwt.TypJWT)
+
+	jws.Payload.Set("role", "admin")
+
+	sign, err := jws.Sign([]byte(hsSecretString))
+	assert.Nil(t, err)
+	assert.Equal(t, []byte("300qEKK0Iibf-_t8rXzVDdBPX-xEXT-9v_sFJ2X4zYU"), sign)
+}
+
+func TestJWS_Sign_HS384_1(t *testing.T) {
 	jws := New()
 	jws.Header.Set(HeaderParamAlg, jwa.AlgHS384)
 	jws.Header.Set(HeaderParamTyp, jwt.TypJWT)
 
 	jws.Payload.Set("role", "admin")
 
-	sign, err := jws.Sign("thisissecret")
+	sign, err := jws.Sign(hsSecretString)
 	assert.Nil(t, err)
 	assert.Equal(t, []byte("z5LD-Pb68M5VN3y61AzPiRN_pa-N2Vcz6i1wONCulZo5AsrATUMGxx4_OKxh1fGo"), sign)
 }
 
-func TestJWS_Sign_HS512(t *testing.T) {
+func TestJWS_Sign_HS384_2(t *testing.T) {
+	jws := New()
+	jws.Header.Set(HeaderParamAlg, jwa.AlgHS384)
+	jws.Header.Set(HeaderParamTyp, jwt.TypJWT)
+
+	jws.Payload.Set("role", "admin")
+
+	sign, err := jws.Sign([]byte(hsSecretString))
+	assert.Nil(t, err)
+	assert.Equal(t, []byte("z5LD-Pb68M5VN3y61AzPiRN_pa-N2Vcz6i1wONCulZo5AsrATUMGxx4_OKxh1fGo"), sign)
+}
+
+func TestJWS_Sign_HS512_1(t *testing.T) {
 	jws := New()
 	jws.Header.Set(HeaderParamAlg, jwa.AlgHS512)
 	jws.Header.Set(HeaderParamTyp, jwt.TypJWT)
 
 	jws.Payload.Set("role", "admin")
 
-	sign, err := jws.Sign("thisissecret")
+	sign, err := jws.Sign(hsSecretString)
+	assert.Nil(t, err)
+	assert.Equal(t,
+		[]byte("d427lJYZVKAxVqnmMEXsWRLWzGUeAVgIWKggs0sbI1le40QbI4e0e3eAYrKwQ0OcBb2DbS6weRllMfYKaPsAHg"),
+		sign)
+}
+func TestJWS_Sign_HS512_2(t *testing.T) {
+	jws := New()
+	jws.Header.Set(HeaderParamAlg, jwa.AlgHS512)
+	jws.Header.Set(HeaderParamTyp, jwt.TypJWT)
+
+	jws.Payload.Set("role", "admin")
+
+	sign, err := jws.Sign([]byte(hsSecretString))
 	assert.Nil(t, err)
 	assert.Equal(t,
 		[]byte("d427lJYZVKAxVqnmMEXsWRLWzGUeAVgIWKggs0sbI1le40QbI4e0e3eAYrKwQ0OcBb2DbS6weRllMfYKaPsAHg"),
