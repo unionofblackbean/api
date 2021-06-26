7af3bda1b8b2c5fadba32f7807b7d24841fb75bf
Leung Yau Ming <leungyauming2005@gmail.com>
2021-06-26 12:21:51 +0800

    create jws.JWS.signHS method and implement HS algorithms in jws.JWS.Sign method

--- common/jws/jws.go
+++ common/jws/jws.go
@@ -1,8 +1,14 @@
 package jws
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"crypto/sha512"
+	"errors"
 	"fmt"
+	"github.com/leungyauming/api/common/jwa"
 	"github.com/leungyauming/api/common/pools"
+	"hash"
 )
 
 type JWS struct {
@@ -37,4 +43,59 @@ func (jws *JWS) encodeHeaderPayload() ([]byte, error) {
 	pools.PutBytesBuffer(buf)
 
 	return headerPayloadEncoded, nil
+}
+
+func (jws *JWS) signHS(iSecret interface{}) ([]byte, error) {
+	var secret []byte
+	switch iSecret.(type) {
+	case string:
+		secret = []byte(iSecret.(string))
+	case []byte:
+		secret = iSecret.([]byte)
+	default:
+		return nil, errors.New("unknown secret data type")
+	}
+
+	var hashFunc func() hash.Hash
+	switch jws.Header.Get("alg") {
+	case jwa.AlgHS256:
+		hashFunc = sha256.New
+	case jwa.AlgHS384:
+		hashFunc = sha512.New384
+	case jwa.AlgHS512:
+		hashFunc = sha512.New
+	}
+
+	headerPayloadEncoded, err := jws.encodeHeaderPayload()
+	if err != nil {
+		return nil, fmt.Errorf("failed to encode header and payload -> %v", err)
+	}
+
+	signHash := hmac.New(hashFunc, secret)
+	_, err = signHash.Write(headerPayloadEncoded)
+	if err != nil {
+		return nil, fmt.Errorf("failed to write jws to sign function -> %v", err)
+	}
+	return signHash.Sum(nil), nil
+}
+
+func (jws *JWS) Sign(secretOrPriKey interface{}) ([]byte, error) {
+	if !jws.Header.Exists(HeaderParamAlg) {
+		return nil, errors.New("missing alg header parameter")
+	}
+
+	var sign []byte
+	var err error
+
+	alg := jws.Header.Get(HeaderParamAlg)
+	switch alg {
+	case jwa.AlgHS256, jwa.AlgHS384, jwa.AlgHS512:
+		sign, err = jws.signHS(secretOrPriKey)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to sign jws -> %v", err)
+	}
+
+	return sign, nil
 }
