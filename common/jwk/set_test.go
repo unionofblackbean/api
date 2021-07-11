8084be621191aaf2bd30b1a4d434374bc4ac9d56
Leung Yau Ming <leungyauming2005@gmail.com>
2021-07-11 11:50:21 +0800

    test: create tests for jwk.Set struct

---
+++ common/jwk/set_test.go
@@ -0,0 +1,44 @@
+package jwk
+
+import (
+	"github.com/leungyauming/api/common/jwa"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewSet(t *testing.T) {
+	set := NewSet()
+	assert.NotNil(t, set)
+	assert.NotNil(t, set.Keys)
+}
+
+func TestSet_AddKey(t *testing.T) {
+	jwk := New()
+	jwk.Set(ParamAlg, jwa.AlgES256)
+
+	set := NewSet()
+	set.AddKey(jwk)
+	assert.ElementsMatch(t, []JWK{jwk}, set.Keys)
+}
+
+func TestSet_RemoveKey(t *testing.T) {
+	jwk := New()
+	jwk.Set(ParamAlg, jwa.AlgES256)
+
+	set := NewSet()
+	set.AddKey(jwk)
+	set.RemoveKey(0)
+	assert.Empty(t, set.Keys)
+}
+
+func TestSet_Build(t *testing.T) {
+	jwk := New()
+	jwk.Set(ParamAlg, jwa.AlgES256)
+
+	set := NewSet()
+	set.AddKey(jwk)
+
+	sj, err := set.Build()
+	assert.Nil(t, err)
+	assert.Equal(t, `{"keys":[{"alg":"ES256"}]}`, string(sj))
+}
