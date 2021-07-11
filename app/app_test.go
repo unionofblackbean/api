2836561e15c56c93ca4ba43f02a61165c44de6f5
Leung Yau Ming <leungyauming2005@gmail.com>
2021-07-11 11:16:43 +0800

    test: create tests for app.App struct

---
+++ app/app_test.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func newApp() *App {
+	deps := &Deps{}
+	return New(deps)
+}
+
+func TestNew(t *testing.T) {
+	app := newApp()
+	assert.NotNil(t, app)
+	assert.NotNil(t, app.services)
+	assert.NotNil(t, app.deps)
+}
+
+type testService struct {
+}
+
+func (s *testService) Start() error {
+	return nil
+}
+
+func (s *testService) Shutdown() error {
+	return nil
+}
+
+func TestApp_RegisterService(t *testing.T) {
+	app := newApp()
+
+	service := &testService{}
+	app.RegisterService(service)
+
+	assert.ElementsMatch(t, []Service{service}, app.services)
+}
