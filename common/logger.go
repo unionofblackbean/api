6c73ad31f132c2977c15d23bdd4a65b323a63e1f
Leung Yau Ming <leungyauming2005@gmail.com>
2021-07-13 13:41:02 +0800

    fix: add brackets to enclose prefix of logger

--- common/logger.go
+++ common/logger.go
@@ -8,5 +8,5 @@ import (
 const Lflags = log.Ldate | log.Ltime | log.Lshortfile | log.Lmsgprefix
 
 func NewLogger(prefix string) *log.Logger {
-	return log.New(os.Stdout, prefix, Lflags)
+	return log.New(os.Stdout, "["+prefix+"] ", Lflags)
 }
