17efec43f59ef60299c1b46eca3b45ed4a3c161c
Leung Yau Ming <leungyauming2005@gmail.com>
2021-07-24 15:55:43 +0800

    test: create test for Start and Shutdown methods of App struct

--- app/app_test.go
+++ app/app_test.go
@@ -1,41 +1,142 @@
 package app
 
 import (
+	"errors"
+	"github.com/leungyauming/api/app/config"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func newApp() *App {
-	deps := &Deps{}
-	return New(deps)
-}
-
 func TestNew(t *testing.T) {
-	app := newApp()
+	app := New(&Deps{})
 	assert.NotNil(t, app)
 	assert.NotNil(t, app.deps)
 }
 
-type testService struct {
+type registerTestService struct {
 }
 
-func (s *testService) Start() error {
+func (s *registerTestService) Start() error {
 	return nil
 }
 
-func (s *testService) Shutdown() error {
+func (s *registerTestService) Shutdown() error {
 	return nil
 }
 
-func (s *testService) Name() string {
-	return "test"
+func (s *registerTestService) Name() string {
+	return ""
 }
 
 func TestApp_RegisterService(t *testing.T) {
-	app := newApp()
+	app := New(&Deps{})
 
-	service := &testService{}
+	service := new(registerTestService)
 	app.RegisterService(service)
 
 	assert.ElementsMatch(t, []Service{service}, app.services)
+}
+
+type exitOnErrorStartTestService struct {
+}
+
+func (s *exitOnErrorStartTestService) Start() error {
+	return errors.New("test error for exit on error policy")
+}
+
+func (s *exitOnErrorStartTestService) Shutdown() error {
+	return nil
+}
+
+func (s *exitOnErrorStartTestService) Name() string {
+	return ""
+}
+
+func TestApp_Start_ExitOnError(t *testing.T) {
+	app := New(&Deps{
+		Config: &config.Config{
+			App: &config.AppConfig{
+				StartPolicy: config.StartPolicyExitOnError,
+			},
+		},
+	})
+	app.RegisterService(new(exitOnErrorStartTestService))
+
+	err := app.Start()
+	assert.NotNil(t, err)
+}
+
+type neverExitStartTestService struct {
+}
+
+func (s *neverExitStartTestService) Start() error {
+	return errors.New("test error for never exit policy")
+}
+
+func (s *neverExitStartTestService) Shutdown() error {
+	return nil
+}
+
+func (s *neverExitStartTestService) Name() string {
+	return ""
+}
+
+func TestApp_Start_NeverExit(t *testing.T) {
+	app := New(&Deps{
+		Config: &config.Config{
+			App: &config.AppConfig{
+				StartPolicy: config.StartPolicyNeverExit,
+			},
+		},
+	})
+	app.RegisterService(new(neverExitStartTestService))
+
+	err := app.Start()
+	assert.Nil(t, err)
+}
+
+type shutdownNoErrorService struct {
+}
+
+func (s *shutdownNoErrorService) Start() error {
+	return nil
+}
+
+func (s *shutdownNoErrorService) Shutdown() error {
+	return nil
+}
+
+func (s *shutdownNoErrorService) Name() string {
+	return ""
+}
+
+func TestApp_Shutdown_NoError(t *testing.T) {
+	app := New(&Deps{})
+	app.RegisterService(new(shutdownNoErrorService))
+
+	errs := app.Shutdown()
+	assert.Equal(t, 0, len(errs))
+}
+
+type shutdownWithErrorService struct {
+}
+
+func (s *shutdownWithErrorService) Start() error {
+	return nil
+}
+
+func (s *shutdownWithErrorService) Shutdown() error {
+	return errors.New("test error")
+}
+
+func (s *shutdownWithErrorService) Name() string {
+	return ""
+}
+
+func TestApp_Shutdown_WithError(t *testing.T) {
+	app := New(&Deps{})
+	app.RegisterService(new(shutdownWithErrorService))
+
+	errs := app.Shutdown()
+	assert.Equal(t, 1, len(errs))
 }
