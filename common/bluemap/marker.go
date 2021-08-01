f3044ccf4fd3b34178b9483f1139380d568d9997
Leung Yau Ming <leungyauming2005@gmail.com>
2021-08-02 00:06:17 +0800

    style: move bluemap.Marker.FillColor field to extrude, shape group

--- common/bluemap/marker.go
+++ common/bluemap/marker.go
@@ -14,27 +14,21 @@ type Marker struct {
 
 	// poi
 	Icon string `json:"icon"`
-
 	// html
 	HTML string `json:"html"`
-
 	// poi, html
 	Anchor linalg.Vec2f32 `json:"anchor"`
-
-	// extrude, shape
-	Shape []ShapeVertex `json:"shape"`
 
 	// extrude
 	ShapeMinY float32 `json:"shapeMinY"`
 	ShapeMaxY float32 `json:"shapeMaxY"`
-
 	// shape
-	ShapeY    float32     `json:"shapeY"`
-	FillColor linalg.RGBA `json:"fillColor"`
-
+	ShapeY float32 `json:"shapeY"`
+	// extrude, shape
+	Shape     []ShapeVertex `json:"shape"`
+	FillColor linalg.RGBA   `json:"fillColor"`
 	// line
 	Lines []linalg.Vec3i `json:"line"`
-
 	// extrude, line, shape
 	Detail    string      `json:"detail"`
 	DepthTest bool        `json:"depthTest"`
