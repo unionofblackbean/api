000fa7c6bdb871712dc94e998f1084ca3bf5fd48
Leung Yau Ming <leungyauming2005@gmail.com>
2021-08-04 21:42:32 +0800

    feat: create bluemap.MarkersDocument.IsValid() method

--- common/bluemap/markers_document.go
+++ common/bluemap/markers_document.go
@@ -3,3 +3,13 @@ package bluemap
 type MarkersDocument struct {
 	MarkerSets []MarkerSet `json:"markerSets"`
 }
+
+func (doc *MarkersDocument) IsValid() bool {
+	for _, set := range doc.MarkerSets {
+		if !set.IsValid() {
+			return false
+		}
+	}
+
+	return true
+}
